35b1515259cf1d0c17c6cb8afebec8c6629ef85f
Panhia Thao <panhiathao@ses-macbook-pro.attlocal.net>
2021-04-22 17:03:45 -0700

    udemy work

---
+++ hmong.js
@@ -0,0 +1,14 @@
+document.querySelector("p")
+
+console.log(12)
+console.log("hello world")
+console.log(document.querySelector("p"))
+
+document.documentElement
+console.log(document.documentElement)
+
+document.documentElement.firstChild
+console.log(document.documentElement.firstChild)
+
+document.documentElement.lastChild
+console.log(document.documentElement.lastChild)
--- index.js
+++ index.js
@@ -5,3 +5,12 @@ console.log("hello world")
 console.log(document.querySelector("p"))
 
 console.log(document.documentElement)
+
+document.documentElement
+console.log(document.documentElement)
+
+document.documentElement.firstChild
+console.log(document.documentElement.firstChild)
+
+document.documentElement.lastChild
+console.log(document.documentElement.lastChild)
