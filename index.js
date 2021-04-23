8f978b32405c9234d95f38bc6f04ddd1038bf230
Panhia Thao <panhiathao@ses-macbook-pro.attlocal.net>
2021-04-22 17:41:53 -0700

    add hmong pages

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-document.querySelector("p")
-
-console.log(12)
 console.log("hello world")
-console.log(document.querySelector("p"))
-
-console.log(document.documentElement)
-
-document.documentElement
-console.log(document.documentElement)
-
-document.documentElement.firstChild
-console.log(document.documentElement.firstChild)
-
-document.documentElement.lastChild
-console.log(document.documentElement.lastChild)
