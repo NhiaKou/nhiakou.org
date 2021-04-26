361a8567b8dcb63ec600a4ae07294184cecbd2dc
Panhia Thao <panhiathao@ses-macbook-pro.attlocal.net>
2021-04-26 15:08:30 -0700

    spelling error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-console.log("hello world")
+//console.log("hello world")
 function updateLanguage(event) {
     //console.log(event.target.value)
-    document.lcation = event.target.value
+    document.location = event.target.value
 }
