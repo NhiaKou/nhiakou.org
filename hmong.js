bae727c2fd2d52ad2de1404c9321870a92b0d1a1
Panhia Thao <panhiathao@ses-macbook-pro.attlocal.net>
2021-04-26 15:04:02 -0700

    add select button and onchange function

--- hmong.js
+++ hmong.js
@@ -1,1 +1,7 @@
 console.log("Nyob zoo ntiaj teb")
+
+function updateLanguage(event) {
+    //console.log(event.target.value)
+    //url: hmong.html
+    document.location= event.target.value
+}
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log("hello world")
+function updateLanguage(event) {
+    //console.log(event.target.value)
+    document.lcation = event.target.value
+}
