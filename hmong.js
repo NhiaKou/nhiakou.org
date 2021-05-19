16e945ceb863ebef1a60a09332b39e8482ff3f35
Panhia Thao <panhiathao@Ses-MacBook-Pro.attlocal.net>
2021-05-18 17:17:11 -0700

    correction on lang button

--- hmong.js
+++ hmong.js
@@ -2,6 +2,9 @@ console.log("Nyob zoo ntiaj teb")
 
 function updateLanguage(event) {
     //console.log(event.target.value)
-    //url: hmong.html
-    document.location= event.target.value
+    document.location = event.target.value
+}
+
+function updateLanguage(event) {
+    document.location = event.target.value
 }
--- index.js
+++ index.js
@@ -3,3 +3,4 @@ function updateLanguage(event) {
     //console.log(event.target.value)
     document.location = event.target.value
 }
+
