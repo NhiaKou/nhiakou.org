027191811f7323e520de6a5831febd19c3283d86
Panhia Thao <panhiathao@Ses-MacBook-Pro.attlocal.net>
2021-05-19 11:36:31 -0700

    add css for mobile

--- hmong.js
+++ hmong.js
@@ -3,9 +3,4 @@
 function updateLanguage(event) {
     //console.log(event.target.value)
     document.location = event.target.value
-<<<<<<< HEAD
 }
-
-=======
-}
->>>>>>> 53059747d3b9dd474f1b10885dc31a79893ae4fa
