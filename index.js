c8820b71eb84d172a8e797e5382820e960761ad4
Thon Ly <thonly@heartbank.ceo>
2022-02-04 18:02:11 -0800

    added google analytics

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateLanguage(event) {
     document.location = event.target.value
 }
 
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VWXGDRL057');
