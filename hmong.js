efb8cc578672a64edfdf16b481a442bb86f8d1ac
Thon Ly <thonly@heartbank.ceo>
2022-10-16 17:46:16 -0700

    add theme

--- hmong.js
+++
@@ -1,6 +0,0 @@
-//console.log("Nyob zoo ntiaj teb")
-
-function updateLanguage(event) {
-    //console.log(event.target.value)
-    document.location = event.target.value
-}
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-//console.log("hello world")
-function updateLanguage(event) {
-    //console.log(event.target.value)
-    document.location = event.target.value
-}
+import "./components/sw-header/element.mjs";
+
+import "./components/sw-main/sw-curriculum/element.mjs";
+import "./components/sw-main/sw-podcast/element.mjs";
+import "./components/sw-main/sw-quiz/element.mjs";
+import "./components/sw-main/sw-code/element.mjs";
+import "./components/sw-main/sw-cohort/element.mjs";
+import "./components/sw-main/sw-coupon/element.mjs";
+import "./components/sw-main/element.mjs";
+
+import "./components/sw-footer/element.mjs";
 
 window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-VWXGDRL057');
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-VWXGDRL057');
