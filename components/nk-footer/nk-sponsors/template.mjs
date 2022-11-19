16d97feca1b26d88b579376102dba5e81fca385c
Thon Ly <thonly@heartbank.ceo>
2022-11-18 18:20:53 -0800

    refactor

--- components/nk-footer/nk-sponsors/template.mjs
+++ components/nk-footer/nk-sponsors/template.mjs
@@ -1,9 +1,9 @@
-import { ORIGIN, NHIAKOU } from "/global.mjs";
+import { ORIGIN, THONLY } from "/global.mjs";
 const template = document.createElement("template");
 
 template.innerHTML = `
-    <link rel="stylesheet" href="${ORIGIN}/components/tl-footer/shadow.css">
-    <link rel="stylesheet" href="${NHIAKOU}/components/nk-footer/nk-sponsors/shadow.css">
+    <link rel="stylesheet" href="${THONLY}/components/tl-footer/shadow.css">
+    <link rel="stylesheet" href="${ORIGIN}/components/nk-footer/nk-sponsors/shadow.css">
     <h5>Sponsors</h5>
     <ul class="donors">
         <li><a href="https://github.com/heartbank"><img src="https://github.com/heartbank.png?size=100"></a></li>
